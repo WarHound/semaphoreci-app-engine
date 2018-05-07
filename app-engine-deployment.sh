663ecb1f6b7985a8cc8f668f16bbc725086e2699
Sergey Tsygulev <s.tsygulev@gmail.com>
2018-05-08 01:48:59 +0300

    Init

---
+++ app-engine-deployment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PATH=$SEMAPHORE_CACHE_DIR/google-cloud-sdk/bin:$PATH
+
+if [ ! -f "$SEMAPHORE_CACHE_DIR/google-cloud-sdk/bin/gcloud" ]
+then
+  curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$VERSION-linux-x86_64.tar.gz
+  tar -x -C $SEMAPHORE_CACHE_DIR -f google-cloud-sdk-$VERSION-linux-x86_64.tar.gz
+  gcloud --quiet components update --version $VERSION
+fi
+
+gcloud auth activate-service-account --key-file=auth_key.json
+
+gcloud app deploy --quiet
